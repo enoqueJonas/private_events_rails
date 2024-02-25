1477cf44fc0bfb5a75079f7330359cbf96577273
enoqueJonas <enoque.macanda@gmail.com>
2024-02-25 17:48:49 +0200

    feat: Add functionality to be able to create new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,24 @@
 class EventsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @event = Event.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @event_creation = EventCreation.new
+    @event = @event_creation.build_created_event(events_params)
+    @event_creation.event_creator = @user
+
+    if @event.save
+      @event_creation.save
+      redirect_to user_events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def events_params
+    params.require(:event).permit(:title, :location, :date)
+  end
 end
