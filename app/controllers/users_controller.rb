abb51b12e8698793221eb795fe3cdb98614562fe
enoqueJonas <enoque.macanda@gmail.com>
2024-03-01 18:16:02 +0200

    feat: Add functionality to display users attended events on users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @events = @user.events
+    @events = Event.joins("INNER JOIN events e, attendances ON attendances.user_id=#{@user.id} AND attendances.event_id=e.id").distinct
   end
 end
